0c2c4067ba65532a4ace8c8e9a895ff34f6585c8
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-04-19 20:09:04 +0200

    header

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <HeaderComponent />
+    <MainComponent />
+  </div>
+</template>
+
+<script>
+import { store } from './data/store';
+import axios from 'axios';
+import HeaderComponent from './components/headerComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    MainComponent
+  },
+  data(){
+    return {
+      store
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="h-top p-2 d-flex justify-content-between align-items-center">
+        <h1 class="text-uppercase text-danger">Boolflix</h1>
+        <div>
+            <input type="text">
+            <button>Search</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderComponent"
+}
+</script>
+
+<style lang="scss" scoped>
+    .h-top{
+        background-color: #121212;
+    }
+</style>
